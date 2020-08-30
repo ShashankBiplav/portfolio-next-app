63eee31ac1c8c6e02c03ca91f5000bffe66affbf
Shashank Biplav <Biplavshashank7@gmail.com>
2020-08-30 23:06:27 +0530

    Added routing to all  Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import Projects from './components/Projects/Projects';
+import Skills from './components/Skills/Skills';
+import Testimonials from './components/Testimonials/Testimonials';
+
 
 function App() {
   return (
     <div className="App">
-
+        <Switch>
+          <Route path="/projects" exact component={Projects}/>
+          <Route path="/skills" exact component={Skills}/>
+          <Route path="/testimonials" exact component={Testimonials}/>
+          <Route path="/" component={Home}/>
+        </Switch>
     </div>
   );
 }
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const projects = (props) => (
+    <div>
+        <h1>Projects</h1>
+    </div>
+);
+
+export default projects;
